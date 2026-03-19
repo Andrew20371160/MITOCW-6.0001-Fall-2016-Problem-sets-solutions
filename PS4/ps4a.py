34cdd4b45a30b2a84515cc87dd1b515974f14b15
Andrew Karam Saber <andrew.k.saber@gmail.com>
2026-03-19 23:44:53 +0200

    Create ps4a.py
    Implement recursive function to generate permutations.

---
+++ PS4/ps4a.py
@@ -0,0 +1,43 @@
+# Problem Set 4A
+# Name: <your name here>
+# Collaborators:
+# Time Spent: x:xx
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+    if len(sequence)==1:
+        return [sequence]
+    else:
+        l = get_permutations(sequence[1:len(sequence)+1])
+        ret_list = []
+        for perm in l:
+            for i in range(len(sequence)):
+                ret_list.append(perm[:i] + sequence[0]+perm[i:])
+        return ret_list
+
+
+if __name__ == '__main__':
+
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+        
+    
