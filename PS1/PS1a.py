14fa949ace48a962e5214a822d4ddf4c80449ca4
Andrew Karam Saber <andrew.k.saber@gmail.com>
2026-03-19 23:40:26 +0200

    Create PS1a.py

---
+++ PS1/PS1a.py
@@ -0,0 +1,20 @@
+portion_down_payment = 0.25
+current_savings = 0 
+r = 0.04
+
+annual_salary= float(input("\nEnter annual salary"))
+
+portion_saved= float(input("\nEnter percent of salary to save"))
+
+total_cost= float(input("\nEnter the cost"))
+
+down_payment = portion_down_payment * total_cost
+
+month_count = 0 
+while current_savings <down_payment:
+	current_savings += portion_saved *(annual_salary/12) + r*(current_savings/12) 
+	month_count+=1
+
+
+print("\n",month_count)
+
